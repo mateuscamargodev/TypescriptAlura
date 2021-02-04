d0d1c3624cab769693f02813711f7c58515970b7
mateus <mateuscamargo929@gmail.com>
2021-02-04 17:45:04 -0300

    Dando início aos estudos de typescript

---
+++ app/ts/models/Negociacao.ts
@@ -0,0 +1,29 @@
+class Negociacao {
+
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
+    }
+
+    getVolume() {
+        return this._quantidade * this._valor;
+    }
+
+}
